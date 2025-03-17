b6ee27614467b7de309f0aa85ba92f9793c44d0b
Cduram12 <duramdaniel@hotmail.com>
2025-03-16 18:30:24 -0600

    Update CY402_G2_CasodeEstudio_1.py
    Nombre del dispositivo agregado

--- CY402_G2_CasodeEstudio_1.py
+++ CY402_G2_CasodeEstudio_1.py
@@ -4,7 +4,7 @@ import usb.util
 def monitoreo():
     try:
         dispositivos = list(usb.core.find(find_all=True))  
-        print(f"Dispositivos USB encontrados: {len(dispositivos)}") 
+        print(f"Dispositivos USB encontrados: {len(dispositivos)}")
 
         if not dispositivos:
             print("No se encontraron dispositivos USB.")
@@ -12,17 +12,23 @@ def monitoreo():
 
         for dispositivo in dispositivos:
             try:
+                nombre_dispositivo = usb.util.get_string(dispositivo, dispositivo.iProduct)
+            except usb.core.USBError:
+                nombre_dispositivo = "No encontrado"
+
+            try:
                 numeroDeSerial = usb.util.get_string(dispositivo, dispositivo.iSerialNumber)
             except usb.core.USBError:
                 numeroDeSerial = "No encontrado"
-            
+
             print(
-                f"Dispositivo encontrado: VendorID={hex(dispositivo.idVendor)}, "
-                f"ProductoID={hex(dispositivo.idProduct)}, Numero de Serial={numeroDeSerial}"
+                f"Dispositivo encontrado: {nombre_dispositivo} | "
+                f"VendorID={hex(dispositivo.idVendor)} | "
+                f"ProductoID={hex(dispositivo.idProduct)} | "
+                f"Numero de Serial={numeroDeSerial}"
             )
     except usb.core.USBError as e:
-        print(f"Error al acceder a dispositivos USB: {e}")
+        print(f"Error al encontradar dispositivos USB: {e}")
 
 if __name__ == "__main__":
     monitoreo()
-
